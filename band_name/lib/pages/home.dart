897e30dcc82cb78e18ab04e779d3254472a1a0bd
Emmanuel <beltreelgg2@gmail.com>
2022-03-15 14:03:53 -0400

    latest changes

--- band_name/lib/pages/home.dart
+++ band_name/lib/pages/home.dart
@@ -41,20 +41,38 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  ListTile bandTile(Band band) {
-    return ListTile(
-      leading: CircleAvatar(
-        child: Text(band.name.substring(0, 2)),
-        backgroundColor: Colors.blue[100],
-      ),
-      title: Text(band.name),
-      trailing: Text(
-        '${band.votes}',
-        style: TextStyle(fontSize: 20),
-      ),
-      onTap: () {
-        print(band.name);
+  Widget bandTile(Band band) {
+    return Dismissible(
+      key: Key(band.id),
+      direction: DismissDirection.startToEnd,
+      onDismissed: (DismissDirection direction) {
+        print('Direccion : $direction');
+        //TODO: llamar el borrado en el server
       },
+      background: Container(
+        padding: EdgeInsets.only(left: 9),
+        color: Colors.red[300],
+        child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Deletle Band',
+              style: TextStyle(color: Colors.white),
+            )),
+      ),
+      child: ListTile(
+        leading: CircleAvatar(
+          child: Text(band.name.substring(0, 2)),
+          backgroundColor: Colors.blue[100],
+        ),
+        title: Text(band.name),
+        trailing: Text(
+          '${band.votes}',
+          style: TextStyle(fontSize: 20),
+        ),
+        onTap: () {
+          print(band.name);
+        },
+      ),
     );
   }
 
